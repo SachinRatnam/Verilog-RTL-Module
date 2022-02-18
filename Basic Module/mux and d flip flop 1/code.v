5089b67118b6b0e18adffa386c856ffb7861d400
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-02-19 00:00:18 +0530

    Update code.v

--- Basic Module/mux and d flip flop 1/code.v
+++ Basic Module/mux and d flip flop 1/code.v
@@ -1,1 +1,15 @@
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+    
+    always @(posedge clk)begin
+        Q <= L ? r_in: q_in ; 
+            
+    end
+        
+
+endmodule
 
