91233731835d44dd8ed23d6bd299115ab8f10b0b
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-02-18 23:42:20 +0530

    Update D latch.v

--- Basic Module/D latch.v
+++ Basic Module/D latch.v
@@ -1,1 +1,11 @@
+module top_module (
+    input d, 
+    input ena,
+    output q);
+    
+    always @(*)
+        if (ena)
+            q = d ; // non blocking assignment is used as it is not triggerd by clock
+
+endmodule
 
