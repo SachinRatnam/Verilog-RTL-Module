8d636d89b498f7bae7231838bfc28cbc167d3798
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-02-20 02:07:00 +0530

    Update Decade_counter.v

--- counter/Decade_counter.v
+++ counter/Decade_counter.v
@@ -1,5 +1,5 @@
 // Build a decade counter that counts from 0 through 9, inclusive, with a period of 10. The reset input is synchronous, and should reset the counter to 0.
-module bcd_counter (
+module decade_counter (
     input clk,
     input reset,        // Synchronous active-high reset
     output reg [3:0] q);
