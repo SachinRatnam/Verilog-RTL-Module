88e31d0eee6f9caa30f45fc5bf57576d7d5c70fe
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-02-20 02:01:53 +0530

    Update convert 1000 to 1 hz freq.v

--- counter/convert 1000 to 1 hz freq.v
+++ counter/convert 1000 to 1 hz freq.v
@@ -1,1 +1,29 @@
+module convert(
+    input in_clk,reset,
+    output out_clk
+);
+    wire [11:0] q_in;
+    
+    bcd_counter C1 (in_clk,reset,q_in[3:0]);        // all ff in C1 is given in_clk , output of C1--  q_in[3] will have freq (in_clk / 10)
+    bcd_counter C2 (q_in[3],reset,q_in[7:4]);       // all ff in C2 in given clk as q_in[3] which is output of 1st bcd_counter C1, output of C2-- q_in[7] will have feq (in_clk / 100 ) 
+    bcd_counter C2 (q_in[7],reset,q_in[11:8]);
+    
+    assign out_clk = q_in;
+    
+endmodule
 
+
+module bcd_counter (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output reg [3:0] q);
+    
+    always @(posedge clk)begin    // it's a synchronous counter 
+        if(reset || q==9)
+            q <= 0;
+        else
+            q <= q + 1;
+        
+    end
+
+endmodule
