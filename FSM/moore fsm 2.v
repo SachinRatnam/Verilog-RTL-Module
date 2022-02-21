47a25bec4ceb679dede21849456279678d63f66b
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-02-21 15:12:26 +0530

    Update and rename FSM/Implement only the state transition logic and output logic/code.v to FSM/moore fsm 2.v

--- FSM/moore fsm 2.v
+++ FSM/moore fsm 2.v
@@ -1,10 +1,17 @@
 module top_module(
-    input in,
-    input [1:0] state,
-    output [1:0] next_state,
-    output out); //
-
+    input in,clk,areset,
+    output out);
+    
+    reg [1:0] state, next_state ;
     parameter A=0, B=1, C=2, D=3;
+    
+ 
+    always @(posedge clk, negedge areset)begin
+        if(! areset)
+            state <= A;
+        else
+            state <= next_state ;
+    end
 
     // State transition logic
     always @(*)begin
