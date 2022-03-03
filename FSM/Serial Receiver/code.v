ab41dfa461737440c6883cc812c2a7d26b5bef9d
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-03-03 18:54:48 +0530

    Update code.v

--- FSM/Serial Receiver/code.v
+++ FSM/Serial Receiver/code.v
@@ -5,16 +5,47 @@ module top_module(
     output done
 ); 
     parameter rst= 0,d_cnt= 1,dn= 2,err= 3;
-    reg [3:0]cnt;
+    reg [2:0]cnt;
     
-    reg [2:0]state, next_state ;
+    reg [1:0]state, next_state ;
     
     always @(posedge clk)begin
-        if(reset)
-            state <= rst ;
-        	cnt <= 0;
+        if(reset)begin
+            	state <= rst ;
+        		cnt <= 0;
+        		end
         else
-            
+            begin
+            	state <= next_state ;
+        		if (next_state == d_cnt)
+                    cnt <= cnt + 1 ;
+                else if (next_state == dn || next_state == err)
+                    cnt <= 0 ;
+            end
+                
     end
+    
+    always @(*)begin
+        // done = 1'b0 ;
+        case(state)
+            rst: next_state = in ? rst: d_cnt ;
+            d_cnt:begin
+                if(cnt== 7 && in)
+                        next_state = dn ;
+                else if (cnt== 7 && (~in))
+                    	next_state = err ;
+                	else
+                        next_state = d_cnt ;
+             	  end
+            dn:begin
+                	next_state = in ? rst: d_cnt ;
+                	// done = 1'b1 ;
+            	end
+            err: next_state = in ? rst: err ;
+            
+        endcase
+    end
+    
+    assign done = (state == dn);
 
 endmodule
