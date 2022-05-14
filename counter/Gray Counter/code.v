6b8b03a37930a1a9fb26051ecc51808f2ce2b38e
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-05-14 16:17:15 +0530

    Create code.v

---
+++ counter/Gray Counter/code.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:00:54 05/14/2022 
+// Design Name: 
+// Module Name:    a 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module a(clk,reset,gray_count);
+	input clk,reset;
+	output [3:0] gray_count;
+	reg [3:0] count;
+	
+	always @(posedge clk,posedge reset)begin
+		if(reset)
+			count <= 0 ;
+		else
+			count = count + 1;
+	end
+	
+	assign gray_count = count ^ count >> 1 ;
+
+
+endmodule
