b1ba08bc6511ff0d70736eb81666822dacf6942d
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-06-10 18:08:39 +0530

    Update 4-bit binary counter.v

--- counter/4-bit binary counter.v
+++ counter/4-bit binary counter.v
@@ -3,13 +3,13 @@
 module counter (
     input clk,
     input reset,      // Synchronous active-high reset
-    output [3:0] q);
+    output reg [3:0] count);
     
     always @(posedge clk)begin
         if (reset)
-            q <= 0;
+            count <= 0;
         else
-            q <= q + 1;
+            count <= count + 1;
     end
 
 endmodule
