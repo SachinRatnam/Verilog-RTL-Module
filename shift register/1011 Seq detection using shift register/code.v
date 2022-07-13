e39eba2cab0bad3a8a99503fd41702ab0ab35491
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-07-13 12:53:34 +0530

    Create code.v

---
+++ shift register/1011 Seq detection using shift register/code.v
@@ -0,0 +1,18 @@
+module main(x,clk,reset,z
+    );
+	 input x,clk,reset ;
+	 output z ;
+	 
+	 reg [3:0] int_ff ;
+	 
+	 always @(posedge clk)begin
+			if (reset)
+				int_ff <= 0 ;
+			else
+				int_ff <= {int_ff[2:0],x} ;
+		end
+		
+		assign z = int_ff[3] & (~int_ff[2]) & int_ff[1] & int_ff[0] ;
+
+
+endmodule
